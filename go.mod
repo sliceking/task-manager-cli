ff77a55d3a54c56f31e8dfcdcfffc955f13add07
svwielga4 <svwielga4@gmail.com>
2019-11-26 14:39:33 -0800

    starting the basics of boltdb

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cobra v0.0.5
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/sliceking/task-manager-cli/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/sliceking/task-manager-cli/cmd"
+	"github.com/sliceking/task-manager-cli/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
